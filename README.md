bcc9bb4cc89a9bdfc25ea4d89ecd4ba76013d3ac
Hoàng Minh Nguyễn <nguyen31hoang08minh2003@gmail.com>
2024-05-07 02:23:43 +0700

    Create WelcomePage

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 CS422 - Software Analysis and Design - Final project
 
 cd large_shelf
+
 npm run dev
---
+++ large_shelf/src/components/WelcomePage.tsx
@@ -0,0 +1,9 @@
+import { Fragment } from "react";
+import { MouseEvent } from "react";
+import { useState } from "react";
+
+function WelcomePage() {
+    
+}
+
+export default WelcomePage;
