e8dd2464c5a784ad08ca01aa34c9f653033db99b
Hoàng Minh Nguyễn <nguyen31hoang08minh2003@gmail.com>
2024-05-07 03:04:39 +0700

    Add cover logo

--- large_shelf/src/components/WelcomePage.tsx
+++ large_shelf/src/components/WelcomePage.tsx
@@ -1,21 +1,41 @@
 import { Fragment } from "react";
 import { MouseEvent } from "react";
 import { useState } from "react";
-import "./../welcome_page_styles.css";
+import "./../styles/welcome_page_styles.css";
+
+function CoverLogo() {
+    return (<img 
+        className = "cover-logo"
+        src = "./../assets/large_shelf_logo.svg"
+        alt = "Large Shelf Logo"
+    />);
+}
+
+function CoverTitle() {
+    return (<h1 
+        className = "cover-title"
+    >
+            You can read your favorite books here, <br/>
+        or listen to them read by your preference of voices
+    </h1>);
+}
 
 function CoverPart() {
     return (<div 
         className = "cover-part"
     >
-        This should be cover part!!!
+        {CoverLogo()}
+        {CoverTitle()}
     </div>);
 }
 
 function WelcomePage() {
     return (
-        <>
+        <div
+            className = "welcome-page"
+        >
             {CoverPart()}
-        </>
+        </div>
     )
 }
 
