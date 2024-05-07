7eb12302401316599a18fdb687bddbb9582128ee
Hoàng Minh Nguyễn <nguyen31hoang08minh2003@gmail.com>
2024-05-07 08:32:39 +0700

    Add Page ID

--- large_shelf/src/App.tsx
+++ large_shelf/src/App.tsx
@@ -1,12 +1,23 @@
 import { useState } from 'react';
 import WelcomePage from './components/WelcomePage';
+import PAGE_ID from './PageID';
 
 function App() {
-  return (
-    <>
-      <WelcomePage />
-    </>
-  );
+    const [pageID, setPageID] = useState(PAGE_ID["WELCOME_PAGE"]);
+    
+    if (pageID === PAGE_ID["WELCOME_PAGE"]) {
+        return (
+            <div>
+                <WelcomePage />
+            </div>
+        );
+    } else {
+        return (
+            <div>
+                <h1>Home Page</h1>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ large_shelf/src/PageID.tsx
@@ -0,0 +1,6 @@
+const PAGE_ID = {
+    WELCOME_PAGE: 0,
+    HOME_PAGE: 1
+};
+
+export default PAGE_ID;
