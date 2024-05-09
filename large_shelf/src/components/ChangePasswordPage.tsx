5da0c00c79ccea5f29fd897a056e00bbd22140ea
Hoàng Minh Nguyễn <nguyen31hoang08minh2003@gmail.com>
2024-05-09 14:38:15 +0700

    Add styles for library page

--- large_shelf/src/components/ChangePasswordPage.tsx
+++ large_shelf/src/components/ChangePasswordPage.tsx
@@ -1,9 +1,302 @@
 import { Fragment, MouseEvent, useState } from "react";
+import PAGE_ID from "../PageID";
+import VerticalPageBar from "./VerticalPageBar";
+import TopHorizontalBar from "./TopHorizontalBar";
+import "../styles/change_password_page_styles.css";
+import defaultProfilePicture from "../assets/default_profile_picture.png";
 
-export default function ChangePasswordPage() {
+interface ChangePasswordPageProps {
+    onPageOptionClick: (pageID: number) => void;
+}
+
+function BriefProfileInformationPart(
+    {
+        profilePicture,
+        fullName
+    }: BriefProfileInformationPartProps
+) {
     return (
-        <div>
-            ChangePasswordPage
+        <div
+            id = "brief-information-part"
+        >
+            <img
+                src = {profilePicture}
+                alt = "Profile Picture"
+                id = "displayed-profile-picture"
+            />
+            <div
+                id = "displayed-full-name"
+            >
+                {fullName}
+            </div>
+        </div>
+    );
+}
+
+function DetailedProfileInformationPart(
+    {
+        firstName,
+        lastName,
+        email,
+        phoneNumber,
+        biography
+    }: DetailedProfileInformationPartProps
+) {
+
+    var [passwordVisibility, setPasswordVisibility] = useState<boolean>(false);
+
+    var password: string = "This is not a password";
+
+    if (passwordVisibility) {
+        /*
+        
+            Request server to get plaintext password
+        
+        */
+       password = "I am the one who knocks!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!";
+    }
+
+    return (
+        <div
+            id = "detailed-information-part"
+        >
+            <button
+                id = "request-save-modification-button"
+            >
+                Save your modification
+            </button>
+
+            <div
+                id = "name-information-row"
+            >
+                <div
+                    id = "first-name-container"
+                    className = "profile-information-container"
+                >
+                    <div
+                        className = "profile-information-label"
+                    >
+                        First name
+                    </div>
+
+                    <input
+                        type = "text"
+                        id = "editable-first-name-box"
+                        className = "editable-profile-box"
+                        placeholder={firstName}
+                    />
+
+                </div>
+
+
+                <div
+                    id = "last-name-container"
+                    className = "profile-information-container"
+                >
+                    <div
+                        className = "profile-information-label"
+                    >
+                        Last name
+                    </div>
+
+                    <input
+                        type = "text"
+                        id = "editable-last-name-box"
+                        className = "editable-profile-box"
+                        placeholder={lastName}
+                    />
+
+                </div>
+            </div>
+
+            <div
+                id = "contact-information-row"
+            >
+                <div
+                    id = "email-container"
+                    className = "profile-information-container"
+                >
+                    <div
+                        className = "profile-information-label"
+                    >
+                        Email
+                    </div>
+
+                    <input
+                        type = "text"
+                        id = "editable-email-box"
+                        className = "editable-profile-box"
+                        placeholder={email}
+                    />
+
+                </div>
+
+
+                <div
+                    id = "phone-number-container"
+                    className = "profile-information-container"
+                >
+                    <div
+                        className = "profile-information-label"
+                    >
+                        Phone number
+                    </div>
+
+                    <input
+                        type = "text"
+                        id = "editable-phone-number-box"
+                        className = "editable-profile-box"
+                        placeholder={phoneNumber}
+                    />
+
+                </div>
+            </div>
+
+            <div
+                id = "password-information-row"
+            >
+                
+                <div
+                    id = "password-container"
+                    className = "profile-information-container"
+                >
+                    <div
+                        className = "profile-information-label"
+                    >
+                        Password
+                    </div>
+
+                    <div
+                        id = "displayed-password-field"
+                        style = {{
+                            fontStyle: passwordVisibility ? "normal" : "italic"
+                        }}
+                    >
+                        <input
+                            id = "password-field-text-part"
+                            type = "text"
+                            readOnly = {true}
+                            style = {{
+                                fontStyle: passwordVisibility ? "normal" : "italic",
+                                opacity: passwordVisibility ? 1 : 0.8
+                            }}
+                            value = {passwordVisibility ? password : "Hidden"}
+                        />
+
+                        <button
+                            id = "password-visibility-button"
+                            onClick = {
+                                (event: MouseEvent<HTMLButtonElement>) => {
+                                    setPasswordVisibility(!passwordVisibility);
+                                }
+                            }
+                        >
+                            <img
+                                src = {passwordVisibility ? openedEyeIcon : closedEyeIcon}
+                                alt = "Password Visibility"
+                            />
+                        </button>
+                        
+                    </div>
+
+                </div>
+
+                <button
+                    id = "change-password-button"
+                >
+                    Change password
+                </button>
+
+            </div>
+
+            <div
+                id = "biography-information-row"
+            >
+                <div
+                    id = "biography-container"
+                    className = "profile-information-container"
+                >
+                    <div
+                        id = "biography-information-label"
+                    >
+                        Biography
+                    </div>
+
+                    <textarea
+                        id = "editable-biography-box"
+                        placeholder={biography}
+                    >
+                    </textarea>
+
+                </div>
+            </div>
         </div>
     )
+}
+
+function ProfileInformationPart() {
+
+    /*
+    
+        Send request to server to get user's information
+    
+    */
+
+    var information = {
+        profilePicture: defaultProfilePicture,
+        fullName: "Nakahara Hiroshi",
+        firstName: "Hiroshi",
+        lastName: "Nakahara",
+        email: "deadlinesarecommingfromeverydirections@gmail.com",
+        phoneNumber: "0123456789",
+        biography: "I am not in danger. I am the danger. I am the one who knocks!!!"
+    };
+
+
+    return (
+        <div
+            id = "information-list-part"
+        >
+            <BriefProfileInformationPart
+                profilePicture = {information.profilePicture}
+                fullName = {information.fullName}
+            />
+            <DetailedProfileInformationPart
+                firstName = {information.firstName}
+                lastName = {information.lastName}
+                email = {information.email}
+                phoneNumber = {information.phoneNumber}
+                biography = {information.biography}
+            />
+        </div>
+    );
+}
+
+export default function ChangePasswordPage(
+    {
+        onPageOptionClick
+    }: ChangePasswordPageProps
+) {
+    return (
+        <div
+            id = "account-page"
+        >
+            <TopHorizontalBar 
+        
+            />
+            <div
+                id = "content-part"
+            >
+                <VerticalPageBar
+                    chosenPageID = {
+                        PAGE_ID.ACCOUNT_PAGE
+                    }
+                    onOptionClick = {
+                        onPageOptionClick    
+                    }
+                />
+                <ProfileInformationPart/>
+            </div>
+        </div>
+    );
 }
--- large_shelf/src/components/VoicePage.tsx
+++ large_shelf/src/components/VoicePage.tsx
@@ -1,9 +1,345 @@
-import { Fragment, MouseEvent, useState } from "react";
+import { Fragment, useState, MouseEvent } from "react";
 
-export default function VoicePage() {
+import PAGE_ID from "../PageID";
+import "../styles/shelf_page_styles.css";
+import VerticalPageBar from "./VerticalPageBar";
+import TopHorizontalBar from "./TopHorizontalBar";
+import InfinieScroll from "react-infinite-scroll-component";
+import duneCoverImage from "../assets/dune_cover_image.png";
+import backButtonIcon from "../assets/back_button_icon.svg";
+
+interface ShelfPageProps {
+    onPageOptionClick: (pageID: number) => void;
+    onShelfBookOptionClick: (bookID: string) => void;
+    shelfID: string;
+}
+
+interface ShelfBookOptionProps {
+    imageLinkOfBookCover: string;
+    title: string;
+    authorName: string;
+    dateAdded: Date;
+    averageRating: number;
+    userProcess: number;
+    genre: string;
+    onClick: (event: MouseEvent) => void;
+}
+
+interface ShelfBookListPartProps {
+    shelfID: string;
+    onPageOptionClick: (pageID: number) => void;
+    onShelfBookOptionClick: (shelfID: string) => void;
+}
+
+function ShelfBookOption(
+    {
+        imageLinkOfBookCover,
+        title,
+        authorName,
+        dateAdded,
+        averageRating,
+        userProcess,
+        genre,
+        onClick
+    }: ShelfBookOptionProps
+) {
     return (
-        <Fragment>
-            <h1>Voice Page</h1>
-        </Fragment>
+        <div
+            className = "shelf-book-option-in-shelf-page"
+        >
+            <button
+                className = "shelf-book-option-detail-in-shelf-page"
+                onClick = {onClick}
+            >
+                <div
+                    className = "detail-cell-in-shelf-page"
+                >
+                    <img
+                        className = "cover-image-of-shelf-book-in-shelf-page"
+                        src = {imageLinkOfBookCover}
+                        alt = "Book cover"
+                    />
+                </div>
+
+                <div
+                    className = "detail-cell-in-shelf-page"
+                >
+                    <h1
+                        className = "title-of-shelf-book-in-shelf-page"
+                    >
+                        Title
+                    </h1>
+                    <p
+                        className = "detail-of-shelf-book-in-shelf-page"
+                    >
+                        {title}
+                    </p>
+                </div>
+
+                <div
+                    className = "detail-cell-in-shelf-page"
+                >
+                    <h1
+                        className = "title-of-shelf-book-in-shelf-page"
+                    >
+                        Author
+                    </h1>
+                    <p
+                        className = "detail-of-shelf-book-in-shelf-page"
+                    >
+                        {authorName}
+                    </p>
+                </div>
+
+                <div
+                    className = "detail-cell-in-shelf-page"
+                >
+                    <h1
+                        className = "title-of-shelf-book-in-shelf-page"
+                    >
+                        Date added
+                    </h1>
+                    <p
+                        className = "detail-of-shelf-book-in-shelf-page"
+                    >
+                        {
+                            `${
+                                (() => {
+                                    const date = dateAdded.getDate();
+                                    const lastDigit = date % 10;
+                                    if ((3 <= date) || (date <= 20)) {
+                                        return `${date}-th`;
+                                    }
+                                    if (lastDigit === 1) {
+                                        return `${date}-st`;
+                                    }
+                                    if (lastDigit === 2) {
+                                        return `${date}-nd`;
+                                    }
+                                    if (lastDigit === 3) {
+                                        return `${date}-rd`;
+                                    }
+                                    return `${date}-th`;
+                                })()
+                            } ${dateAdded.toLocaleString('default', { month: 'long' })},`
+                        }
+                        <br/>
+                        {
+                            dateAdded.getFullYear()
+                        }
+                    </p>
+                </div>
+
+                <div
+                    className = "detail-cell-in-shelf-page"
+                >
+                    <h1
+                        className = "title-of-shelf-book-in-shelf-page"
+                    >
+                        Average rating
+                    </h1>
+                    <p
+                        className = "detail-of-shelf-book-in-shelf-page"
+                    >
+                        {averageRating}/5 stars
+                    </p>
+                </div>
+
+                <div
+                    className = "detail-cell-in-shelf-page"
+                >
+                    <h1
+                        className = "title-of-shelf-book-in-shelf-page"
+                    >
+                        Your process
+                    </h1>
+                    <p
+                        className = "detail-of-shelf-book-in-shelf-page"
+                    >
+                        {(userProcess * 100).toFixed(2)}%
+                    </p>
+                </div>
+
+                <div
+                    className = "detail-cell-in-shelf-page"
+                >
+                    <h1
+                        className = "title-of-shelf-book-in-shelf-page"
+                    >
+                        Genre
+                    </h1>
+                    <p
+                        className = "detail-of-shelf-book-in-shelf-page"
+                    >
+                        {genre}
+                    </p>
+                </div>
+
+            </button>
+        </div>
+    )
+}
+
+const demoShelfBookOptionList = [
+    {
+        bookID: "1",
+        imageLinkOfBookCover: duneCoverImage,
+        title: "Dune",
+        authorName: "Frank Herbert",
+        dateAdded: new Date(2024, 3, 24),
+        averageRating: 4.27,
+        userProcess: 0.9669,
+        genre: "Science Fiction"
+    }
+]
+
+function ShelfBookListPart(
+    {
+        shelfID,
+        onPageOptionClick,
+        onShelfBookOptionClick
+    }: ShelfBookListPartProps
+) {
+    /*
+        Request the number of books in the shelve from the server
+    */
+    const numberOfBooks = demoShelfBookOptionList.length;
+    /*
+    
+        Request from the server to get the shelf name given shelf ID
+    
+    */
+    const shelfName = "Current reading";
+
+    var [shelfBookOptionList, setShelfBookOptionList] = useState(demoShelfBookOptionList);
+    var [hasMore, setHasMore] = useState(true);
+
+    return (
+        <div
+            id = "shelf-book-list-part"
+        >
+            <div 
+                id = "title-bar-in-shelf-page"
+            >
+                <button
+                    id = "back-button-in-shelf-page"
+                    onClick = {
+                        () => {
+                            onPageOptionClick(PAGE_ID.HOME_PAGE);
+                        }
+                    }
+                >
+                    <img
+                        id = "back-button-icon-in-shelf-page"
+                        src = {backButtonIcon}
+                        alt = "Back"
+                    />
+                </button>
+
+                <h1
+                    id = "shelf-name-title-in-shelf-page"
+                > 
+                    {`"${shelfName}" Shelf`} 
+                </h1>
+            </div>
+            <div
+                id = "shelf-book-option-list"
+            >
+                <InfinieScroll
+                        
+                    dataLength = {
+                        shelfBookOptionList.length 
+                    }
+
+                    next = {
+                        () => {
+                            if (shelfBookOptionList.length < numberOfBooks) {
+                                setTimeout(() => {
+                                    /*
+                                    
+                                        Request from server to get more books
+                    
+                                    */
+                                }, 100); 
+                            } else {
+                                setHasMore(false);
+                            }
+                        }
+                    }
+
+                    hasMore = {hasMore}
+
+                    loader = {<p> Loading ... </p>}
+
+                    endMessage = {
+                        <p> End of the list </p>
+                    }
+
+                    scrollableTarget = "shelf-book-option-list"
+                >   
+                    {
+                        shelfBookOptionList.map((item, index) => {
+                            return (
+                                <ShelfBookOption
+                                    imageLinkOfBookCover = {item.imageLinkOfBookCover}
+                                    title = {item.title}
+                                    authorName = {item.authorName}
+                                    dateAdded = {item.dateAdded}
+                                    averageRating = {item.averageRating}
+                                    userProcess = {item.userProcess}
+                                    genre = {item.genre}
+                                    onClick = {
+                                        (event: MouseEvent) => {
+                                            onShelfBookOptionClick(item.bookID);
+                                        }
+                                    }
+                                />
+                            )
+                        })
+                    }
+                </InfinieScroll>
+            </div>
+        </div>
+    );
+}
+
+export default function VoicePage(
+    {
+        onPageOptionClick,
+        onShelfBookOptionClick,
+        shelfID
+    }: ShelfPageProps
+) {
+    return (
+        <div
+            id = "shelf-page"
+        >
+            <TopHorizontalBar 
+        
+            />
+            <div
+                id = "content-part"
+            >
+                <VerticalPageBar
+                    chosenPageID = {
+                        PAGE_ID.HOME_PAGE
+                    }
+                    onOptionClick = {
+                        onPageOptionClick    
+                    }
+                />
+                <ShelfBookListPart
+                    shelfID = {shelfID}
+
+                    onPageOptionClick = {
+                        onPageOptionClick
+                    }
+
+                    onShelfBookOptionClick = {
+                        onShelfBookOptionClick
+                    }
+                />
+            </div>
+        </div>
     );
 }
